c33028a5344d4c1ce8b2fa46cf2f404ef382d596
andrew-home <atquick@outlook.com>
2020-01-05 21:22:02 -0500

    adding learning material

--- cogs/admin.py
+++ cogs/admin.py
@@ -9,8 +9,11 @@ class AdminCog(commands.Cog):
     @commands.has_role('Admin')
     async def add_channel(self, name, chantype='text'):
         # await self.bot.create_channel(name, chantype)
+        pass
 
-
+    # @commands.command(name='sum')
+    # async def sum_nums(self, ctx, num1: int, num2: int):
+    #     await ctx.channel.send(num1 + num2)
 
 def setup(bot):
     bot.add_cog(AdminCog(bot))
--- cogs/main.py
+++ cogs/main.py
@@ -5,7 +5,15 @@ class MainCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='sum', help='Sum of two numbers. Usage: !sum <int> <int>')
+    async def sum_nums(self, ctx, num1, num2):
 
+        print(ctx.content.author.rsplit('#', 1))
+
+        # try:
+        #     await ctx.channel.send(f'@{ctx.content.author.rsplit('#', 1)} the sum is: {int(num1) + int(num2)}')
+        # except:
+        #     await ctx.channel.send('Please try again. Proper usage: !sum <int> <int>')
 
 def setup(bot):
     bot.add_cog(MainCog(bot))
